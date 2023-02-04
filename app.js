96428b17b259c2e66c7721ae99cd6f578c755f19
Santiago Vargas <santiagovargasve@gmail.com>
2023-02-03 23:10:55 -0500

    REFACTOR: Timeout

--- app.js
+++ app.js
@@ -2,17 +2,26 @@ import * as spotify from "./utils/spotify.js";
 import * as twitter from "./utils/twitter.js";
 let timeOut = 3_000;
 
-const entryPoint = () => {
-  setTimeout(async () => {
+const millisToMinutesAndSeconds = (millis) => {
+  var minutes = Math.floor(millis / 60000);
+  var seconds = ((millis % 60000) / 1000).toFixed(0);
+  return minutes + ":" + (seconds < 10 ? "0" : "") + seconds;
+};
+
+const entryPoint = async () => {
+  try {
     const { playing, progress, duration, error, stringSong } =
       await spotify.seeCurrentTrack();
     twitter.updateBio({ playing, error, stringSong });
-    entryPoint();
     timeOut = !error && playing ? duration - progress : 3_000;
 
     console.log("timeout:" + timeOut);
-    console.log("till next song:" + Math.floor((timeOut / 1000) % 60));
-  }, timeOut);
+    console.log(millisToMinutesAndSeconds(timeOut));
+    setTimeout(entryPoint, timeOut);
+  } catch (error) {
+    console.log(error);
+    setTimeout(entryPoint, timeOut);
+  }
 };
 
 entryPoint();
